ae56fe573240895f335ea5baf55e45a3453b01eb
GranCacioRomano <falconetti.grafico@gmail.com>
2025-06-20 13:03:37 +0200

    Impostata la struttura base del gestionale scolastico con menu principale e opzioni iniziali.

---
+++ Gestionale.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Gestionale {
+    private static HashMap<String, ArrayList<Integer>> RegistroStudentiVoti = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean continua = true;
+
+        while (continua) {
+            System.out.println("MENU GESTIONALE");
+            System.out.println("1. AGGIUNGI STUDENTE");
+            System.out.println("2. MODIFICA STUDENTE O VOTO");
+            System.out.println("3. ELIMINA STUDENTE");
+            System.out.println("4. VISUALIZZA STUDENTI");
+            System.out.println("5. ESCI");
+            System.out.println("SCEGLI UNA OPZIONE: ");
+
+            int scelta = scanner.nextInt();
+
+            switch (scelta) {
+                case 1 -> aggiungiStudente();
+                case 2 -> modificaStudente();
+                case 3 -> eliminaStudente();
+                case 4 -> visualizzaStudenti();
+                case 5 -> {
+                    continua = false;
+                    System.out.println("Arrivederci!");
+                }
+                default -> System.out.println("Opzione non valida!");
+            }     
+        }
+    }
+}
